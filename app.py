5c65f353b4a659b35c9ae4de2c613434259b63ea
Alex230877 <122993772+Alex230877@users.noreply.github.com>
2024-11-11 23:32:30 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,28 +1,33 @@
 import psycopg2
+from time import sleep
 
-# Установление соединения с базой данных PostgreSQL
-conn = psycopg2.connect(
-    dbname="PostgreSQL_Sokolov", 
-    user="your_surname", 
-    password="your_password", 
-    host="db", 
-    port="5432"
-)
+for attempt in range(10):
+    try:
+        conn = psycopg2.connect(
+            dbname="PostgreSQL_Sokolov",
+            user="your_surname",
+            password="your_password",
+            host="db",
+            port="5432"
+        )
+        break
+    except psycopg2.OperationalError:
+        print("PostgreSQL не готов, повтор через 5 секунд...")
+        sleep(5)
+else:
+    raise Exception("Не удалось подключиться к базе данных.")
 
 cursor = conn.cursor()
 
-# Запрос для поиска минимального и максимального возраста для имен длиной менее 6 символов
 cursor.execute("""
     SELECT MIN(age), MAX(age)
     FROM test_table
     WHERE LENGTH(name) < 6;
 """)
 
-# Получение результата и вывод в терминал
 result = cursor.fetchone()
 print(f"Минимальный возраст: {result[0]}")
 print(f"Максимальный возраст: {result[1]}")
 
-# Закрытие соединения
 cursor.close()
 conn.close()
